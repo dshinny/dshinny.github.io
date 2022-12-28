9290f571cdcfef9d13d7c32a85663ec0c1c31447
Daniel Shin <daniel990402@gmail.com>
2022-12-28 08:22:51 -0500

    changed href

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,29 +2,31 @@ import React, { useState } from 'react';
 import { FaAlignJustify } from 'react-icons/fa';
 
 const NavBar = () => {
-  const [state, setState] = useState(true);
+  const [open, setOpen] = useState(true);
   return (
     <nav className="navbar">
       <div className="container">
         <div className="navbar-container">
           <ul className="navbar-left">
             <div className="navbar-left-logo">
-              <img src="./image/ds-logo.png" alt=""/>
+              <a href="#home">
+                <img src="./image/ds-logo.png" alt=""/>
+              </a>
             </div>
           </ul>
-          {state ? (
+          {open ? (
             <ul className="navbar-right">
-              <li><a href="#header">Home</a></li>
+              <li><a href="#home">Home</a></li>
               <li><a href="#about">About</a></li>
               <li><a href="#projects">Projects</a></li>
-              <li><a href="#footer">Contact</a></li>
+              <li><a href="#contact">Contact</a></li>
             </ul>
           ) : (
             ""
           )}
         </div>
       </div>
-      <div className="toggle" onClick={() => setState(!state)}>
+      <div className="toggle" onClick={() => setOpen(!open)}>
         <FaAlignJustify />
       </div>
     </nav>
